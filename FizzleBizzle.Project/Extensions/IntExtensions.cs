222cbbd7564c427a69d4dc839d37096a12d3f915
Dana Asbury <danaasbury@gmail.com>
2017-05-30 14:00:10 -0500

    Adding extension method for integers to test divisibility

---
+++ FizzleBizzle.Project/Extensions/IntExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzleBizzle.Extensions
+{
+    public static class IntExtensions
+    {
+        /// <summary>
+        /// IsDivisible is an integer extension method which returns true if and only if a is evenly divisible by n.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static bool IsDivisible(this int a, int n)
+        {
+            return (a % n) == 0;
+        }
+    }
+}
