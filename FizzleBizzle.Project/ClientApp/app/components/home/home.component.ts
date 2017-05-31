867537db3c78d3d50a7be5319f1c25014985f900
daaasbu <danaasbury@gmail.com>
2017-05-31 03:07:13 -0500

    Adding http requests for fizz buzz along with logic to conditionally display the output

--- FizzleBizzle.Project/ClientApp/app/components/home/home.component.ts
+++ FizzleBizzle.Project/ClientApp/app/components/home/home.component.ts
@@ -1,21 +1,48 @@
-import { Component } from '@angular/core';
-import { FizzBuzzForm } from '../models/FizzBuzzForm'
+import { Component, Inject } from '@angular/core';
+import { FizzBuzzForm } from '../models/FizzBuzzForm';
+import { Http } from '@angular/http';
+
+import axios from 'axios';
+
 @Component({
     selector: 'home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
     bazzValue = '';
     predicates = ['Predicate', '>', '>=', '<', '<=', '==', '!='];
     model = new FizzBuzzForm(null, null, null, null, null, null);
     bazzEnabled = false;
+    generateClicked = false;
+    output: string[] = [];
 
     submitted = false;
+
+
+
+
 
     onSubmit() { this.submitted = true; }
 
     public generateOnClick() {
-        console.log(this.bazzEnabled);
+        if (this.bazzEnabled) {
+            axios.get(location.origin + '/api/fizzbuzzbazz/' + this.model.fizz + '/' + this.model.buzz + '/' + this.model.bazz + '/' + this.model.op + '/' + this.model.start + '/' + this.model.end)
+                .then(value => {
+                    this.output = value.data;
+                    this.generateClicked = true;
+                })
+        }
+        else {
+            axios.get(location.origin + '/api/fizzbuzz/' + this.model.fizz + '/' + this.model.buzz + '/' + this.model.start + '/' + this.model.end)
+                .then(value => {
+                    this.output = value.data;
+                    this.generateClicked = true;
+                });
+
+        }
+
     }
+
 }
